6a922be230f4d6b771c51e3924bbf3b47ba997ff
Akbar Khan <akbarbuneri@gmail.com>
2021-08-08 01:20:37 +0300

    Initial commit

---
+++ QuickMapper/AlternateName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickMapper
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class AlternateName : Attribute
+    {
+        public List<string> Names { get; set; }
+        public AlternateName(string name)
+        { 
+            Names = new List<string>();
+            Names.Add(name);
+        }
+        public AlternateName(List<string> names)
+        { 
+            Names = names;
+        }
+    }
+}
---
+++ QuickMapper/Mapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace QuickMapper
+{
+    public static class Mapper
+    {
+        public static T Map<T>(this object model)
+        {
+            T poco = Activator.CreateInstance<T>();
+            foreach (PropertyInfo pi in model.GetType().GetProperties())
+            {
+                if (poco.GetType().GetProperty(pi.Name) != null)
+                {
+                    poco.GetType().GetProperty(pi.Name).SetValue(poco, pi.GetValue(model));
+                }
+            }
+            return poco;
+        }
+    }
+}
